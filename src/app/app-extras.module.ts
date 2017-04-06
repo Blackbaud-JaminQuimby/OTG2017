1439024e61d6716196ac5a83c2abcae417842d4c
Jamin Quimby <jamin.quimby@blackbaud.me>
2017-04-06 12:18:10 -0400

    added firebase

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAiHb8ByUNWBdeKZWIZyUapBMxSggLiJIg",
+  authDomain: "otg2017-f4d23.firebaseapp.com",
+  databaseURL: "https://otg2017-f4d23.firebaseio.com",
+  projectId: "otg2017-f4d23",
+  storageBucket: "otg2017-f4d23.appspot.com",
+  messagingSenderId: "131069844360"
+};
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
+  imports: [
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
   providers: [],
   entryComponents: []
 })
